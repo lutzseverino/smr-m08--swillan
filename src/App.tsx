e29c29467824f9042795ce1b22c947f831e44d6b
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-05-12 16:33:47 +0200

    Set up react project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, HashRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+            
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+

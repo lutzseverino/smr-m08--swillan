7612728223701b80297c0e3ec0eced52dcbc9cba
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-05-20 02:01:43 +0200

    Abstract course block component Clean up dummy data Fix style and create animations

---
+++ src/components/courseblock/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface CourseBlockProps {
+  id: number;
+  title: string;
+  children: React.ReactNode;
+  image: string;
+}
+
+export default class CourseBlock extends React.Component<CourseBlockProps, {}> {
+  render(): React.ReactNode {
+    return (
+      <div
+        className={
+          "flex flex-col overflow-hidden rounded-2xl border-2 border-black bg-columbia-blue transition-all hover:-translate-y-1 hover:-translate-x-1 hover:shadow-solid md:h-48 md:flex-row"
+        }
+      >
+        <div className="w-full">
+          <img
+            className="h-52 w-full object-cover "
+            src={this.props.image}
+            alt={this.props.title + " image"}
+          />
+        </div>
+        <div className="flex flex-col md:min-w-[60%] md:max-w-[60%]">
+          <h4 className="px-8 pt-8">{this.props.title}</h4>
+          <div className="h-full overflow-hidden from-black via-black bg-clip-text px-8 pb-8 md:bg-gradient-to-b md:pb-0 md:text-transparent">
+            <p className="">{this.props.children}</p>
+          </div>
+          <button className="relative bottom-4 ml-auto mr-4 rounded-2xl border-2 border-black bg-rhythm px-4 py-2 text-white transition-colors hover:bg-tropical-violet">
+            Learn more
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import CourseBlock from "../../components/courseblock";
 
-interface State {
+interface HomeState {
   availableCourses: {
     id: number;
     title: string;
     description: string;
-    image?: string;
+    image: string;
   }[];
 }
 
-export default class Home extends React.Component<{}, State> {
+export default class Home extends React.Component<{}, HomeState> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -24,118 +25,68 @@ export default class Home extends React.Component<{}, State> {
         {
           id: 1,
           title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           image: "dummy.png",
         },
         {
           id: 2,
           title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           image: "dummy.png",
         },
         {
           id: 3,
           title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           image: "dummy.png",
         },
         {
           id: 4,
           title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           image: "dummy.png",
         },
         {
           id: 5,
           title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 6,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 7,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 8,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 9,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 10,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
-          image: "dummy.png",
-        },
-        {
-          id: 11,
-          title: "Dummy course",
-          description: "This course doesn't exist, this is a dummy course",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           image: "dummy.png",
         },
       ],
     };
   }
 
-  componentDidMount(): void {
-    console.table(this.state.availableCourses);
-  }
-
-  // TODO: Export button to external component
   render(): React.ReactNode {
     return (
-      <div className="px-8">
-        <h1>
-          Learn, <br />
-          without gates
-        </h1>
-        <h3>
-          Find begginer courses to jump start your programming journey. Without
-          even logging in!
-        </h3>
+      <div className="px-8 md:px-16">
+        <div className="py-8 md:py-32">
+          <h1>
+            <span className="underline">Learn</span>, without gates
+          </h1>
+          <h3>
+            Find begginer courses to jump start your programming journey.
+            Without even logging in!
+          </h3>
+        </div>
 
-        <div className="py-8">
+        <div>
           <h4>Recommended courses</h4>
 
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 2xl:grid-cols-3">
             {this.state.availableCourses.map((course) => (
-              <div
+              <CourseBlock
                 key={course.id}
-                className={
-                  "flex flex-col overflow-hidden rounded-2xl border-[1px] border-black bg-pale-pink transition-all md:h-48 md:flex-row"
-                }
+                id={course.id}
+                image={course.image}
+                title={course.title}
               >
-                <div className="w-full">
-                  <img
-                    className="h-52 w-full object-cover "
-                    src={course.image}
-                    alt={course.title + " image"}
-                  />
-                </div>
-                <div className="flex flex-col md:min-w-[60%] md:max-w-[60%]">
-                  <h4 className="px-8 pt-8">{course.title}</h4>
-                  <div className="h-full overflow-hidden from-black via-black bg-clip-text px-8 pb-8 md:bg-gradient-to-b md:pb-0 md:text-transparent">
-                    <p className="">{course.description}</p>
-                  </div>
-                  <button className="relative bottom-4 ml-auto mr-4 rounded-2xl bg-cadet px-4 py-2 text-white shadow-xl transition-colors hover:bg-pewter-blue">
-                    Learn more
-                  </button>
-                </div>
-              </div>
+                {course.description}
+              </CourseBlock>
             ))}
           </div>
         </div>
